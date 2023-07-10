4d575571810fc49a3288707010d62f59893239f6
Donovan Martinez <88170488+public-space@users.noreply.github.com>
2023-07-09 23:25:05 -0700

    ADD calculate_time_since_last_dose function

--- kratom_tracking_app/functions/user_interaction.py
+++ kratom_tracking_app/functions/user_interaction.py
@@ -1,7 +1,12 @@
 import time
 import random
+from colorama import Fore, Style
+
+
+#?---------------------Menu Functions---------------------# 
 
 def display_menu(): 
+    time.sleep(1)
     print("\n\n")
     print("+++ === +++ === +++ === +++ === +++")
     print("=== +++ Kratom Tracking App +++ ===")
@@ -11,6 +16,7 @@ def display_menu():
     print("3. Calculate Analytics")
     print("0. Exit")
     print("+++ === +++ === +++ === +++ === +++\n\n")
+    time.sleep(0.5)
      
 def process_menu_choice(choice): 
     if choice == "1":
@@ -30,7 +36,10 @@ def process_menu_choice(choice):
         print("Invalid choice boyo. Please try again. :)")
         print("INVALID CHOICE MY GUY --- INVALID CHOICE MY GUY ---")
         print("+++ === +++ === +++ === +++ ==\n")
-        
+        time.sleep(0.5)
+       
+#?---------------------Dose  Functions---------------------# 
+
 def log_dose():
     print("=== +++ Log Dose +++ ===")
     
@@ -51,6 +60,18 @@ def log_dose():
     
     time.sleep(0.5) #? Add a delay to make the output more readable
 
+def calculate_time_since_last_dose(last_dose_timestamp):
+    last_dose_time = datetime.strptime(last_dose_timestamp, "%Y-%m-%d %H:%M:%S")
+    
+    time_difference = datetime.now() - last_dose_time
+    
+    seconds = time_difference.total_seconds()
+    minutes = seconds / 60
+    hours = minutes / 60
+    
+    return f"{int(hours)} hours, {int(minutes % 60)} minutes, {int(seconds % 60)} seconds"
+    
+    
 def view_dose_data(): 
     #? Implement the log to view dose data
     pass
@@ -58,6 +79,10 @@ def view_dose_data():
 def calculate_analytics():
     #?Implement the logic to calculate analytics
     pass
+
+
+#?---------------------Quote---------------------# 
+
 
 def print_encouraging_quote():
     quotes = [
@@ -73,9 +98,12 @@ def print_encouraging_quote():
     
     quote = random.choice(quotes)
     print("+++ === +++ === +++ === +++ === +++")
-    print("=== +++ Encouraging Quote: +++ ===")
+    print(Fore.GREEN + "=== +++ Encouraging Quote: +++ ===" + Style.RESET_ALL)
     print(f"{quote}\n")
     time.sleep(0.5)
+
+#?---------------------Main---------------------# 
+
 
 if __name__ == "__main__":
     display_menu()
