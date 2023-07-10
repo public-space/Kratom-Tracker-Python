e18f35365dc49d2f48e1cde13adb3957f6f160f8
Donovan Martinez <88170488+public-space@users.noreply.github.com>
2023-07-09 21:59:44 -0700

    ADD analytics.py

---
+++ kratom_tracking_app/functions/analytics.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+def calculate_total_doses(): 
+    connection = sqlite3.connect("../database/kratom.db")
+    cursor = connection.cursor()
+    
+    #? Count the number of rows in the 'doses' table
+    
+    cursor.execute("SELECT COUNT(*) FROM doses")
+    total_doses = cursor.fetchone()[0]
+    
+    connection.close()
+    
+    return total_doses
+
+def calculate_dose_frequency():
+    connection = sqlite3.connect("../database/kratom.db")
+    cursor = connection.cursor()
+    
+    #!TODO 
+    #? Query to calculate dose frequency
+    
+    connection.close()
+    
+    return dose_frequency
